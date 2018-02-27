8174f89cc8136077ed1f3da9bb676603eb55d383
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2018-02-26 22:07:14 -0300

    creating the class category

---
+++ src/main/java/com/ufcg/si1/model/enumerations/DiscountType.java
@@ -0,0 +1,31 @@
+package com.ufcg.si1.model.enumerations;
+
+import exceptions.NonExistentObjectException;
+
+public enum DiscountType {
+        NO_DISCOUNT(0.0),
+        GOOD_DISCOUNT(0.9),
+        GREAT_DISCOUNT(0.25),
+        SUPER_DISCOUNT(0.5);
+
+        private final double discountMultiplier;
+
+        DiscountType(double discountMultiplier) {
+            this.discountMultiplier = discountMultiplier;
+        }
+
+        public double getDiscountMultiplier() {
+            return this.discountMultiplier;
+        }
+
+
+        
+        public static DiscountType getByDiscountMultiplier(double discountMultiplier) throws NonExistentObjectException {
+            for (DiscountType discountType : DiscountType.values()) {
+                if (discountType.getDiscountMultiplier() == discountMultiplier) {
+                    return discountType;
+                }
+            }
+            throw new NonExistentObjectException("Invalid Discount Type");
+        }
+}
