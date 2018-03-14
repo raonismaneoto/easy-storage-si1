c490559ab9d551103e2390ac9878c2b742926c91
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-03-14 09:12:47 -0300

    Adding products' quantity to saleDetails and fixing discount html

--- src/main/webapp/app/core/main/controllers/saleDetailsDialogController.js
+++ src/main/webapp/app/core/main/controllers/saleDetailsDialogController.js
@@ -6,10 +6,16 @@
     app.controller('SaleDetailsDialogController', function SaleDetailsDialogController(sale, $uibModalInstance) {
         var saleDetailsCtrl = this;
         saleDetailsCtrl.sale = sale;
-        console.log(sale);
 
         saleDetailsCtrl.close = function () {
             $uibModalInstance.close(201);
         };
-    });
+
+        saleDetailsCtrl.getQuantity = function getQuantity(product) {
+            var pair = _.find(saleDetailsCtrl.sale.productsQuantity, function(pair) {
+                return pair.barCode === product.barCode;
+            });
+            return pair.quantity;
+        };
+    }); 
 })();
